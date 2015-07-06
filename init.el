799b70355c25abc1d12f1f886528fac62b7ab757
Bill Piel <bill@billpiel.com>
2015-07-05 21:42:37 -0400

    set rainbow color scheme for rainbow delimiters

--- init.el
+++ init.el
@@ -292,6 +292,19 @@
       (switch-to-buffer (other-buffer (current-buffer) 1)))
 (global-set-key (kbd  "C-M-<return>") 'switch-to-most-recent-buffer)
 
+;; https://github.com/clojure-emacs/cider#basic-configuration
+(setq cider-auto-select-error-buffer nil)
+
+(set-face-attribute 'rainbow-delimiters-depth-1-face nil :foreground "#BB2222")
+(set-face-attribute 'rainbow-delimiters-depth-2-face nil :foreground "#BB7700")
+(set-face-attribute 'rainbow-delimiters-depth-3-face nil :foreground "#BBBB22")
+(set-face-attribute 'rainbow-delimiters-depth-4-face nil :foreground "#11BB11")
+(set-face-attribute 'rainbow-delimiters-depth-5-face nil :foreground "#33BBBB")
+(set-face-attribute 'rainbow-delimiters-depth-6-face nil :foreground "#5555AA")
+(set-face-attribute 'rainbow-delimiters-depth-7-face nil :foreground "#AA55AA")
+(set-face-attribute 'rainbow-delimiters-depth-8-face nil :foreground "#BBBBBB")
+(set-face-attribute 'rainbow-delimiters-depth-9-face nil :foreground "#66FF66")
+
 ;; END Bill's stuff
 
 (message "\n\n Pack loading completed. Your Emacs is Live...\n\n")
