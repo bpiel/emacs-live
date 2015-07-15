76e79a6901a455a0b2d8bd224fb36d85c49822fd
Bill Piel <bill@billpiel.com>
2015-07-15 04:43:38 -0400

    C-c s d = insert-spyd

--- init.el
+++ init.el
@@ -307,16 +307,21 @@
 (set-face-attribute 'rainbow-delimiters-depth-9-face nil :foreground "#66FF66")
 
 (defun sudo-edit (&optional arg)
-  "Edit currently visited file as root.
-
-With a prefix ARG prompt for a file to visit.
-Will also prompt for a file to visit if current
-buffer is not visiting a file."
+  "Edit currently visited file as root. With a prefix ARG prompt
+for a file to visit. Will also prompt for a file to visit if
+current buffer is not visiting a file."
   (interactive "P")
   (if (or arg (not buffer-file-name))
       (find-file (concat "/sudo:root@localhost:"
                          (ido-read-file-name "Find file(as root): ")))
     (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
+(defun insert-spyd ()
+  (interactive)
+  (insert "#spy/d")
+  (live-delete-whitespace-except-one))
+
+(global-set-key (kbd "C-c s d") 'insert-spyd)
 
 
 ;; END Bill's stuff
